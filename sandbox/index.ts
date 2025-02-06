cf6eeade3190704b17011ac96e6bff0dbed40f3b
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-02-06 16:40:11 +0530

    pipe and map redis commands

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,12 +2,28 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1951
 	});
 
-	const car = await client.hGetAll('car#32');
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1952
+	});
+
+	await client.hSet('car3', {
+		color: 'yellow',
+		year: 1953
+	});
+
+	const results = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+
+	console.log(results);
 };
 
 run();
