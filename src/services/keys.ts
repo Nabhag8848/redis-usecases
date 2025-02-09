a085e9833602a4a30bd1cb7a049d175d7c300096
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-02-09 18:57:43 +0530

    view per user

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -25,3 +25,5 @@ export const itemsKey = (itemId: string) => {
 export const itemsByViewsKey = () => `items:views`;
 
 export const itemsByEndingAtKey = () => `items:endingAt`;
+
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`
