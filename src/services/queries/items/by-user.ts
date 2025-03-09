daf0a603e02ed82b7f9e9c26415d8d0c1c48e4f9
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-03-10 01:07:12 +0530

    search and sort including direction per page

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,3 +1,7 @@
+import { itemsIndexKey, itemsKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -5,4 +9,26 @@ interface QueryOpts {
 	direction: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	const query = `@ownerId:{${userId}}`;
+	const sortCriteria = opts.direction &&
+		opts.sortBy && {
+			BY: opts.sortBy,
+			DIRECTION: opts.direction
+		};
+
+	const { total, documents } = await client.ft.search(itemsIndexKey(), query, {
+		ON: 'HASH',
+		SORTBY: sortCriteria,
+		LIMIT: {
+			from: opts.page * opts.perPage,
+			size: opts.perPage
+		}
+	} as any);
+	return {
+		totalPages: Math.ceil(total / opts.perPage),
+		items: documents.map(({ id, value }) => {
+			return deserialize(id.replace(itemsKey(''), ''), value as any);
+		})
+	};
+};
