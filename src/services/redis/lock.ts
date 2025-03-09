20488c8d33e83cbbd15514c10deb1ad1166ab0ba
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-03-09 15:28:59 +0530

    fix: concurrency client so that other engineers don't need to use signal everytime

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,6 +1,6 @@
 import { randomBytes } from 'crypto';
 import { client } from './client';
-export const withLock = async (key: string, cb: (signal: any) => any) => {
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
 	const retryDelayms = 100;
 	let retries = 20;
 
@@ -25,7 +25,9 @@ export const withLock = async (key: string, cb: (signal: any) => any) => {
 			setTimeout(() => {
 				signal.expired = true;
 			}, 2000);
-			const result = await cb(signal);
+
+			const proxiedClient = buildClientProxy(2000)
+			const result = await cb(proxiedClient, signal);
 			return result;
 		} finally {
 			await client.unlock(lockKey, token);
@@ -33,7 +35,24 @@ export const withLock = async (key: string, cb: (signal: any) => any) => {
 	}
 };
 
-const buildClientProxy = () => {};
+type Client = typeof client;
+
+const buildClientProxy = (timeoutMs) => {
+	const startTime = Date.now();
+
+	const handler = {
+		get(target: Client, prop: keyof Client) {
+			if (Date.now() >= startTime + timeoutMs) {
+				throw new Error('Lock has expired');
+			}
+
+			const value = target[prop];
+			return typeof value === 'function' ? value.bind(target) : value;
+		}
+	};
+
+	return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
